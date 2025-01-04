dc720cad928f74f965dbcaf1604b2fd75230528b
Jaydon Turner <75629676+turne309@users.noreply.github.com>
2025-01-04 17:01:36 -0500

    post-vacay move

--- starter/dev-data/data/import-dev-data.js
+++ starter/dev-data/data/import-dev-data.js
@@ -1,31 +1,42 @@
-const fs = require('fs');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const Tour = require('./../../models/tourModel');
+const fs = require("fs");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const Tour = require("./../../models/tourModel");
+const User = require("./../../models/userModel");
 // to lines below have to be in this order (lesson 67)
 
-dotenv.config({ path: './config.env' });
+dotenv.config({ path: "./config.env" });
 
 // Connecting to the mongoDB
 const DB = process.env.DATABASE.replace(
-  '<PASSWORD>',
+  "<PASSWORD>",
   process.env.DATABASE_PASSWORD
-).replace('<DBNAME>', process.env.DBNAME);
+).replace("<DBNAME>", process.env.DBNAME);
 mongoose.connect(DB).then((con) => {
   // console.log(con.connection);
-  console.log('DB connection successfull!');
+  console.log("DB connection successfull!");
 });
 
 // Read JSON file
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
-);
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, "utf-8"));
+
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, "utf-8"));
 
 // Import data into database
-const importData = async () => {
+const importDataTOUR = async () => {
   try {
     await Tour.create(tours);
-    console.log('Data successfully imported!');
+    console.log("Data successfully imported!");
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+const importDataUSER = async () => {
+  try {
+    await User.create(users);
+    console.log("Data successfully imported!");
   } catch (err) {
     console.log(err);
   }
@@ -33,24 +44,39 @@ const importData = async () => {
 };
 
 // Delete all data from collection
-const deleteData = async () => {
+const deleteDataTOUR = async () => {
   try {
     await Tour.deleteMany();
-    console.log('Data successfully deleted!');
+    console.log("Data successfully deleted!");
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+const deleteDataUSER = async () => {
+  try {
+    await User.deleteMany();
+    console.log("Data successfully deleted!");
   } catch (err) {
     console.log(err);
   }
   process.exit();
 };
 
-if (process.argv[2] === '--import') {
-  console.log('Attempting to import data...');
-  importData();
-} else if (process.argv[2] === '--delete') {
-  console.log('Attempting to delete data...');
-  deleteData();
+if (process.argv[2] === "--importTOUR") {
+  console.log("Attempting to import tour data...");
+  importDataTOUR();
+} else if (process.argv[2] === "--importUSER") {
+  console.log("Attempting to import user data...");
+  importDataUSER();
+} else if (process.argv[2] === "--deleteTOUR") {
+  console.log("Attempting to delete tour data...");
+  deleteDataTOUR();
+} else if (process.argv[2] === "--deleteUSER") {
+  console.log("Attempting to delete user data...");
+  deleteDataUSER();
 } else {
-  console.log('Neither operation ran.');
+  console.log("Neither operation ran.");
 }
 
 console.log(process.argv);
